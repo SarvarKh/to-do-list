44a0def5c6da44ab0fb8d073155298ff514476f5
Sarvar Khalimov <sarvarkhalimov@Sarvars-MacBook-Air.local>
2021-05-26 18:17:05 +0500

    Add displayHeading and displayTask

--- src/default.js
+++ src/default.js
@@ -32,12 +32,18 @@ function submitForm(btn) {
         const dueDate = document.querySelector('#dueDate');
         const priority = document.querySelector('#priority');
         const notes = document.querySelector('#notes');
-        const checklist = document.querySelector('#checklist');
+        let checklist = document.querySelector('#checklist');
 
+        if (checklist.checked) {
+            checklist.status = "Closed";
+        } else {
+            checklist.status = "Open";
+        }
+        console.log(checklist["data-value"]);
         if (title.value === '' || description.value === '' || dueDate.value === '' || priority.value === '' || notes.value === '') {
             alert('Fields must be filled out'); // eslint-disable-line no-alert
           } else {
-            const newTask = new Task(title.value, description.value, dueDate.value, priority.value, notes.value, checklist.value);
+            const newTask = new Task(title.value, description.value, dueDate.value, priority.value, notes.value, checklist.status);
             addTaskToDefaultTasks(newTask, allTasks);
             displayTasks(newTask);
         }
@@ -116,10 +122,80 @@ function displayTaskForm() {
     return form;
 }
 
+function displayTableHeading() {
+    const table = createE("table");
+    const trHeading = createE("tr");
+    const trh1 = createE("th", "Title");
+    const trh2 = createE("th", "Description");
+    const trh3 = createE("th", "Due Date");
+    const trh4 = createE("th", "Priority");
+    const trh5 = createE("th", "Notes");
+    const trh6 = createE("th", "Status");
+    trHeading.appendChild(trh1);
+    trHeading.appendChild(trh2);
+    trHeading.appendChild(trh3);
+    trHeading.appendChild(trh4);
+    trHeading.appendChild(trh5);
+    trHeading.appendChild(trh6);
+    table.appendChild(trHeading);
+
+    return table;
+}
+
+const displayTasks = (newTask) => {
+    const table = document.querySelector('table');
+
+    const tr = document.createElement('tr');
+    tr.classList.add('task-row');
+  
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const td5 = document.createElement('td');
+    const td6 = document.createElement('td');
+    const removeBtn = document.createElement('button');
+    const td6Btn = document.createElement('button');
+  
+    td1.textContent = newTask.title;
+    td2.textContent = newTask.description;
+    td3.textContent = newTask.dueDate;
+    td4.textContent = newTask.priority;
+    td5.textContent = newTask.notes;
+    td6Btn.textContent = newTask.checklist;
+    removeBtn.textContent = 'Remove';
+    tr.setAttribute('data-index', allTasks.default.indexOf(newTask));
+  
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    td6.appendChild(td6Btn);
+    td6.appendChild(removeBtn);
+    table.appendChild(tr);
+  
+    td6Btn.addEventListener('click', () => {
+      if (td6Btn.textContent === 'Closed') {
+        td6Btn.textContent = 'Open';
+      } else {
+        td6Btn.textContent = 'Closed';
+      }
+    });
+  
+    removeBtn.addEventListener('click', () => {
+      tr.remove();
+    });
+
+    return table;
+};
+
 function createDefault() {
     const defaultPage = createE("div", false, "default-page");
 
     defaultPage.appendChild(displayTaskForm());
+    defaultPage.appendChild(displayTableHeading());
 
     return defaultPage;
 }
