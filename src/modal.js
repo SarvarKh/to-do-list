f6bcdd08320e825e2c0ba33c5e029bc1ab31d24e
Sarvar Khalimov <sarvarkhalimov@Sarvars-MacBook-Air.local>
2021-06-07 21:59:37 +0500

    Add a form to edit project modal

--- src/modal.js
+++ src/modal.js
@@ -4,9 +4,22 @@ const createProjectModal = () => {
     const modal = createE("div", false, "modal");
     const modalContent = createE('div', false, 'modal-content');
     const close = createE('span', '&times;', "close");
-    const modalText = createE('p', 'Some text in the Modal..');
     modalContent.appendChild(close);
-    modalContent.appendChild(modalText);
+
+    const projectBtn = createE('button');
+    projectBtn.classList.add('crete-project-container');
+    const projectTitle = createE('input');
+    projectTitle.setAttribute('type', 'text');
+    projectTitle.setAttribute('id', 'projectTitleAfterEdit');
+    projectTitle.setAttribute('name', 'projectTitle');
+    projectTitle.setAttribute('placeholder', 'Edit Project');
+  
+    const projectIcon = createE('button', 'Save', 'edit-save-btn');
+    // createProject(projectIcon);
+    projectBtn.appendChild(projectTitle);
+    projectBtn.appendChild(projectIcon);
+    modalContent.appendChild(projectBtn);
+
     modal.appendChild(modalContent);
 
     close.addEventListener('click', () => {
