baed2f2c1eee55b68b6d042feadf51e723f7ca23
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-06-22 22:41:15 +0500

    Re-add babelrc to run jest on webpack improts and exports

--- src/logic.test.js
+++ src/logic.test.js
@@ -1,5 +1,5 @@
-const sum = require('./logic');
+const createE = require("./logic");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+test('Create html element', () => {
+    // test some stuff ds
+})
