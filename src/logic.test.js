5bd77e3185ff62098d49ed5489fc0509e7f221bf
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-06-23 18:09:21 +0500

    Write a test for createE func

--- src/logic.test.js
+++ src/logic.test.js
@@ -1,5 +1,17 @@
-const createE = require("./logic");
+/**
+ * @jest-environment jsdom
+ */
 
-test('Create html element', () => {
-    // test some stuff ds
+const {createE} = require("./logic");
+
+test('Creates a p element', () => {
+    const newElement = document.createElement('p');
+    expect(createE('p')).toStrictEqual(newElement);
+})
+
+test('Creates a div element with its attributes like: textContent, ClassName', () => {
+    const newElement = document.createElement('div');
+    newElement.textContent = "this is the div element content";
+    newElement.classList.add('div-class')
+    expect(createE('div', "this is the div element content", "div-class")).toStrictEqual(newElement);
 })
